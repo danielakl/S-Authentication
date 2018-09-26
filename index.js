69b60698c1c6df84eab3953d186a5e27eb72fbb9
Daniel Klock <daniel.a.klock@gmail.com>
2018-09-26 13:52:00 +0200

    Add client-side login script. Add client-side pbkdf2 encryption.

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 "use strict";
 
-import express from "express";
-import bodyParser from "body-parser";
-import path from "path";
-import crypto from "crypto";
-import mysql from "mysql2";
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const crypto = require("crypto");
+const mysql = require("mysql2");
 
 const server = express();
 server.use(express.static(path.join(__dirname, "public")));
 server.use(bodyParser.urlencoded({extended: true}));
 
-const dbConn = mysql.createConnection({host: 'localhost', user: 'root', database: 'auth'})
-    .then(() => {
-        console.log("Connected to MySQL server.");
-    }).catch(() => {
-        console.error("Failed to connect to MySQL server.");
-    });
+// const dbConn = mysql.createConnection({host: 'localhost', user: 'root', database: 'auth'});
+
 
 server.get("/", (req, res) => {
-    res.sendFile("./views/index.html");
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 server.post("/login", (req, res) => {
@@ -42,5 +38,5 @@ server.use("*", (req, res) => {
 });
 
 server.listen(process.env.PORT | 3000, process.env.IP | "localhost", () => {
-    console.log(`Authentication server listening to request made to 'http://${server.address().address}' at port '${server.address().port}'`);
+    console.log(`Authentication server listening to request made to 'http://localhost' at port '3000'`);
 });
--- public/login.js
+++ public/login.js
@@ -1,2 +1,37 @@
 var form = document.querySelector("#loginForm");
-form
+var usernameInput = document.querySelector("#usernameInput");
+var passwordInput = document.querySelector("#passwordInput");
+var body = document.querySelector("body");
+var salt = CryptoJS.lib.WordArray.random(128/8);
+console.log(salt);
+
+form.onsubmit = function(event) {
+    event.preventDefault();
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/login", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onload = function (e) {
+        if (this.readyState === 4) {
+            var response = JSON.parse(this.responseText);
+            if (this.status === 200) {
+                body.innerHTML = response.message;
+                console.log(response.message);
+            } else {
+                body.innerHTML = response.message;
+                console.error(response.message);
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+        var response = JSON.parse(this.responseText);
+        body.innerHTML = response.message;
+        console.error(response.message);
+    };
+    xhr.send(JSON.stringify({
+        username: usernameInput.value,
+        hash: CryptoJS.PBKDF2(passwordInput.value, salt, { keySize: 512/32, iterations: 1000 })
+    }));
+
+    console.log(usernameInput.value);
+    console.log(CryptoJS.PBKDF2(passwordInput.value, salt, { keySize: 512/32, iterations: 1000 }));
+};
