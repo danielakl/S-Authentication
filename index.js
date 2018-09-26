a42ce14bf94e687023d5fd66155bede5b0a876e0
Daniel Klock <danielkl@stud.ntnu.no>
2018-09-26 18:56:43 +0200

    Comment-out MySQL connection, using JSON file instead. Add comments. Change JSON response data.

--- index.js
+++ index.js
@@ -3,73 +3,77 @@
 const express = require("express");
 const path = require("path");
 const crypto = require("crypto");
-const mysql = require("mysql2");
+// const mysql = require("mysql2/promise");
 const fs = require('fs');
 
-const server = express();
-server.use(express.static(path.join(__dirname, "public")));
-server.use(express.json());
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
-/*let dbConn;
-try {
-dbConn = mysql.createConnection({host: 'localhost', user: 'root', database: 'auth'});
-} catch (e) {
-console.error(e);
-}
-*/
+// Connect to MySQL database.
+// let dbConn;
+// mysql.createConnection({host: 'localhost', user: 'root', database: 'auth'})
+//     .then(connection => {
+//         dbConn = connection;
+//         console.log("Connected to MySQL server.")
+//     }).catch(() => {
+//         console.error("Failed to connect to MySQL server.")
+//     });
 
-server.get("/", (req, res) => {
 
-
-  if (true){
-    res.sendFile(path.join(__dirname, "views", "index.html"));
-  } else {
-    res.sendFile(path.join(__dirname, "views", "home.html"));
-  }
+// Index route
+app.get("/", (req, res) => {
+    if (true){
+        res.sendFile(path.join(__dirname, "views", "index.html"));
+    } else {
+        res.sendFile(path.join(__dirname, "views", "home.html"));
+    }
 });
 
-server.post("/login", (req, res) => {
-  const {username, hash} = req.body;
-  //console.dir(req.body);
-  console.log(`Username: ${username}\nHash: ${hash}`);
-  if (!username || !hash) {
-    res.status(400).json({
-      code: 400,
-      status: "Bad Request",
-      message: "Username and password needed."
-    });
-    return;
-  } else {
-    var users = JSON.parse(fs.readFileSync("./user.json", "UTF-8"));
+// Login route
+app.post("/login", (req, res) => {
+    const {username, hash} = req.body;
+    console.log(`Username: ${username}\nHash: ${hash}`);
+    if (!username || !hash) {
+        res.status(400).json({
+            code: 400,
+            status: "Bad Request",
+            message: "Missing username or password."
+        });
+    } else {
+        let users = JSON.parse(fs.readFileSync("./user.json", "UTF-8"));
 
-    users.forEach(function(el){
-      console.log("else username:" + el.username);
-      if (el.username === username){
-        var salt = el.salt;
-        var hash2 = crypto.pbkdf2Sync(hash, salt, 2048, 512/32, "sha256").toString("hex");
-        console.log(el.hash);
-        console.log(hash2);
-        if (hash2 === el.hash){
-          return res.json({"loggedIn": "YES"});
-        }
-
-      }
-    })
-
-    res.status(404).json({
-      code:404,
-      status: "Password or username is wrong",
-      message: "Bad password/username"
-    });
-
-
-  }
+        users.forEach((el) => {
+            console.log("else username:" + el.username);
+            if (el.username === username){
+                let salt = el.salt;
+                let hash2 = crypto.pbkdf2Sync(hash, salt, 2048, 512/32, "sha256").toString("hex");
+                console.log(el.hash);
+                console.log(hash2);
+                if (hash2 === el.hash){
+                    return res.json({
+                        code: 200,
+                        status: "OK",
+                        message: "Authorization succeeded."
+                    });
+                }
+            }
+        });
+        res.status(401).json({
+            code: 401,
+            status: "Unauthorized",
+            message: "Username or password is wrong."
+        });
+    }
 });
 
-server.use("*", (req, res) => {
-  res.status(404).send("Not Found.");
+// Server 404 page for any unhandled routes.
+app.use("*", (req, res) => {
+    res.status(404).send("Not Found.");
 });
 
-server.listen(process.env.PORT | 3000, process.env.IP | "localhost", () => {
-  console.log(`Authentication server listening to request made to 'http://localhost' at port '3000'`);
+// Start up server listening to requests made to an IP and port.
+const server = app.listen(process.env.PORT | 3000, process.env.IP | "localhost", () => {
+    const {address, port} = server.address();
+    console.log(`Authentication server listening to request made to 'http://${(address === '::') ? "localhost" : address}:${port}'`);
 });
