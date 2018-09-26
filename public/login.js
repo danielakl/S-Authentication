defabeb9566b592864715cb5c4d860676ca6c407
krisnyb <krisnyb@stud.ntnu.no>
2018-09-26 14:05:09 +0200

    hash function returns string now

--- public/login.js
+++ public/login.js
@@ -29,9 +29,9 @@ form.onsubmit = function(event) {
     };
     xhr.send(JSON.stringify({
         username: usernameInput.value,
-        hash: CryptoJS.PBKDF2(passwordInput.value, salt, { keySize: 512/32, iterations: 1000 })
+        hash: CryptoJS.PBKDF2(passwordInput.value, salt, { keySize: 512/32, iterations: 1000 }).toString()
     }));
 
     console.log(usernameInput.value);
-    console.log(CryptoJS.PBKDF2(passwordInput.value, salt, { keySize: 512/32, iterations: 1000 }));
+    console.log(CryptoJS.PBKDF2(passwordInput.value, salt, { keySize: 512/32, iterations: 1000 }).toString());
 };
