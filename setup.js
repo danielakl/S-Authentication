27d0279175c00077846f31cad5dbefaf3e5e5c56
Daniel Klock <danielkl@stud.ntnu.no>
2018-09-27 20:49:51 +0200

    Add generation of 'SECRET_COOKIE' hash for signing cookies.

--- setup.js
+++ setup.js
@@ -3,13 +3,17 @@
 const crypto = require("crypto");
 const fs = require('fs');
 
-crypto.randomBytes(48, (err, secret) => {
-    secret = secret.toString("hex");
-    const data = `SECRET_TOKEN=${secret}`;
-    fs.writeFile("./.env", data, "utf8", err => {
-        if (err) {
-            throw err;
-        }
-        console.log("Generated '.env' file.");
+crypto.randomBytes(48, (err, secretToken) => {
+    secretToken = secretToken.toString("hex");
+    crypto.randomBytes(48, (err, secretCookie) => {
+        secretCookie = secretCookie.toString("hex");
+        const data = `SECRET_TOKEN=${secretToken}\n` +
+                     `SECRET_COOKIE=${secretCookie}`;
+        fs.writeFile("./.env", data, "utf8", err => {
+            if (err) {
+                throw err;
+            }
+            console.log("Generated '.env' file.");
+        });
     });
 });
